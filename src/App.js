8315b3abe4ebe217a650c1317fdfd8a5622e4eb3
Ivan Stantic <ivan.stantic@reversinglabs.com>
2021-02-09 18:23:45 +0100

    Moved gistList state to the App component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 
-import styled from 'styled-components'
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
 import Header from "./components/Header";
 import GistList from "./components/GistList";
 import GlobalStyles from "./GlobalStyle";
+import { getPublicGists } from "./services/gistService";
 
 const App = () => {
+  const [gistList, updateGistList] = useState([]);
+
+  useEffect(() => {
+    fetchGistList();
+  }, []);
+
+  const fetchGistList = async () => {
+    try {
+      const { data: gistList } = await getPublicGists();
+      updateGistList(gistList);
+    } catch(error) {
+      console.log(error);
+    }
+  };
+
   return (
     <Wrapper className="App" data-testid="app">
       <Header />
-      <GistList />
+      <GistList gistList={gistList} />
       <GlobalStyles />
     </Wrapper>
   );
--- src/components/GistList.js
+++ src/components/GistList.js
@@ -1,32 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Gist from "./Gist";
-import { getPublicGists } from "../services/gistService";
 
-const GistList = () => {
-  const [gistList, updateGistList] = useState([]);
-
-  useEffect(() => {
-    fetchGistList();
-  }, []);
-
-  const fetchGistList = async () => {
-    try {
-      const { data: gistList } = await getPublicGists();
-      updateGistList(gistList);
-    } catch(error) {
-      console.log(error);
-    }
-  };
-
-  return (
-    <>
-      {gistList.length > 0 ?
-        <>
-          {gistList.map(gist => <Gist key={gist.id} gist={gist} />)}
-        </>
-      : <h1>Loading</h1>}
-    </>
-  );
-}
+const GistList = ({gistList}) => (
+  <>
+    {gistList.length > 0 ?
+      <>
+        {gistList.map(gist => <Gist key={gist.id} gist={gist} />)}
+      </>
+    : <h1>Loading</h1>}
+  </>
+);
 
 export default GistList;

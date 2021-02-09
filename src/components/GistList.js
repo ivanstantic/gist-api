0c7c627bd14c05b71e07be5692f2cb8babecb428
Ivan Stantic <ivan.stantic@reversinglabs.com>
2021-02-09 17:47:31 +0100

    Added getPublicGists to the GistList component and printed initial results

--- src/components/GistList.js
+++ src/components/GistList.js
@@ -1,4 +1,31 @@
 import React, { useEffect, useState } from 'react'
-const GistList = () => <></>
+import { getPublicGists } from "../services/gistService";
+
+const GistList = () => {
+  const [gistList, updateGistList] = useState([]);
+
+  useEffect(() => {
+    fetchGistList();
+  }, []);
+
+  const fetchGistList = async () => {
+    try {
+      const { data: gistList } = await getPublicGists();
+      updateGistList(gistList);
+    } catch(error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      {gistList.length > 0 ?
+        <ul>
+          {gistList.map(gist => <li key={gist.id}>{gist.id}</li>)}
+        </ul>
+      : <h1>Loading</h1>}
+    </>
+  );
+}
 
 export default GistList
